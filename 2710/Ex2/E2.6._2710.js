cb1e65be8aabc14de9fde1ae3ee804fc169bd95d
Huỳnh Minh Trí <92298736+Harukaru1@users.noreply.github.com>
2022-10-28 23:41:52 +0700

    Delete 2710/Ex2 directory

--- 2710/Ex2/E2.6._2710.js
+++
@@ -1,7 +0,0 @@
-function getSize()
-{
-var w = document.documentElement.clientWidth;
-var h = document.documentElement.clientHeight;
-    
- document.getElementById('wh').innerHTML = "<h1>Width: " + w + " & Height: " + h + "</h1>";
-}
--- 2710/Ex2/Ex2.1_2710.js
+++
@@ -1,3 +0,0 @@
-function updateButton() {
-    document.getElementById("btn").textContent="Haru";
-}
