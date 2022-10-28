83f7b32e0cebf658a27228a41a97cda1b67a18fa
Huỳnh Minh Trí <92298736+Harukaru1@users.noreply.github.com>
2022-10-28 23:23:12 +0700

    Update

---
+++ 2710/Ex5/login.js
@@ -0,0 +1,10 @@
+function formvalid() {
+    var vaildpass = document.getElementById("pass").value;
+  
+    if (vaildpass.length < 8 || vaildpass.length >= 20) {
+      document.getElementById("vaild-pass").innerHTML = "Minimum 8 characters";
+      return false;
+    } else {
+      document.getElementById("vaild-pass").innerHTML = "";
+    }
+}
